0041fc4e12481f795ad6d0b7541b1a8f5e7b76a3
rafaelsouza <rafah.alves12@outlook.com.br>
2024-11-24 23:20:36 -0400

    deploy do projeto

---
+++ src/routes/postsRoutes.js
@@ -0,0 +1,31 @@
+import express from "express";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost} from "../controllers/postController.js";
+import cors from "cors";
+
+const corsOption = {
+    origin: "htpp:localhost:8000",
+    optionsSuccessStatus: 200
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'upload/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage })
+
+const routes = (app) =>{
+    app.use(express.json());
+    app.use(corsOption);
+    app.get("/posts", listarPosts);
+    app.post("/posts", postarNovoPost );
+    app.post("/upload", upload.single("imagem"), uploadImagem )
+    app.put("/upload/:id", atualizarNovoPost)
+}
+
+export default routes;
